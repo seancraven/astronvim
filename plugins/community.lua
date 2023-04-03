8a949280a5b7d9e3e842d87fe064d5c3558d3b06
seancraven <seanpietercraven@gmail.com>
2023-04-03 10:57:31 +0100

    inital commit

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,34 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.nightfox",   enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa",   enabled = false },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight", enabled = true },
+  { import = "astrocommunity.completion.copilot-lua" },
+  {
+    -- further customize the options set by the community
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-j>",
+          prev = "<M-k>",
+          dismiss = "<M-h>",
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -10,3 +10,4 @@ return {
   --   end,
   -- },
 }
+-- some comment
